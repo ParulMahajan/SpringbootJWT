6d589059bd400035389a8b7c36435fb75029713e
AZURE_PARUL\ADT032 <parul@azurethailand.com>
2020-06-30 15:07:28 +0700

    Added Jenkins file

--- Docker/ELK/build.sh
+++ Docker/ELK/build.sh
@@ -1,3 +1,3 @@
-sudo sysctl -w vm.max_map_count=262144
+sysctl -w vm.max_map_count=262144
 
-sudo docker run -p 5601:5601 -p 9200:9200 -p 5044:5044 -p 5120:5120 -it -v /vagrant/sharedFolder/10-syslog.conf:/etc/logstash/conf.d/10-syslog.conf -v /vagrant/sharedFolder/02-beats-input.conf:/etc/logstash/conf.d/02-beats-input.conf --name elk sebp/elk
+docker run -p 5601:5601 -p 9200:9200 -p 5044:5044 -p 5120:5120 -it -v /var/lib/jenkins/workspace/DockerPipeline/Docker/ELK/10-syslog.conf:/etc/logstash/conf.d/10-syslog.conf -v /var/lib/jenkins/workspace/DockerPipeline/Docker/ELK/02-beats-input.conf:/etc/logstash/conf.d/02-beats-input.conf --name elk sebp/elk
