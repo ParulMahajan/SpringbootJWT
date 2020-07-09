a9677fc6e4d9151e29c1b4f0efdb9342eaf142b3
AZURE_PARUL\ADT032 <parul@azurethailand.com>
2020-07-09 11:30:27 +0700

    Removing compose/stack

--- Docker/Kafka/stream-base/runtime/1_start_zookeeper.sh
+++ Docker/Kafka/stream-base/runtime/1_start_zookeeper.sh
@@ -1,3 +1,4 @@
 #This script will start zookeeper service
 #Eg. docker stack deploy -c <path_to_compose_file> <service_name>
-sudo docker stack deploy -c docker-compose-zookeeper.yaml kafkaz
+#sudo docker stack deploy -c docker-compose-zookeeper.yaml kafkaz
+sudo docker run d -p 2181:2181 mahajan777/kafka-zookeeper:latest
