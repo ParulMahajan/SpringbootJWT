a66da6158f3c0d0c06089e87854ee0d24088b791
AZURE_PARUL\ADT032 <parul@azurethailand.com>
2020-07-17 11:34:07 +0700

    created kafka cluster

--- Docker/Kafka/stream-base/runtime/2_start_kafka.sh
+++ Docker/Kafka/stream-base/runtime/2_start_kafka.sh
@@ -4,6 +4,6 @@
 
 docker run -d -p 9092:9092 --name kafkaNode --network mynetwork  -v /var/lib/jenkins/workspace/DockerPipeline/Docker/Kafka/kafkaNode/artifacts/server.properties:/apps/kafka/config/server.properties  mahajan777/kafka-node:latest
 
-docker run -d -p 9093:9093 --name kafkaNode1 --network mynetwork  -v /var/lib/jenkins/workspace/DockerPipeline/Docker/Kafka/kafkaNode/artifacts/server1.properties:/apps/kafka/config/server.properties  mahajan777/kafka-node:latest
+docker run -d -p 9093:9092 --name kafkaNode1 --network mynetwork  -v /var/lib/jenkins/workspace/DockerPipeline/Docker/Kafka/kafkaNode/artifacts/server1.properties:/apps/kafka/config/server.properties  mahajan777/kafka-node:latest
 
-docker run -d -p 9094:9094 --name kafkaNode2 --network mynetwork  -v /var/lib/jenkins/workspace/DockerPipeline/Docker/Kafka/kafkaNode/artifacts/server2.properties:/apps/kafka/config/server.properties  mahajan777/kafka-node:latest
+docker run -d -p 9094:9092 --name kafkaNode2 --network mynetwork  -v /var/lib/jenkins/workspace/DockerPipeline/Docker/Kafka/kafkaNode/artifacts/server2.properties:/apps/kafka/config/server.properties  mahajan777/kafka-node:latest
